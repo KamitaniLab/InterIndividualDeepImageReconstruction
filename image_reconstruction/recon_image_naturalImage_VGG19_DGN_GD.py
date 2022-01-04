34157d2d9cf87adb6e02348df8a018aeed4b2c7e
Kai <junkai125@gmail.com>
2022-01-04 13:44:31 +0900

    renamed filename

--- image_reconstruction/recon_image_naturalImage_VGG19_DGN_GD.py
+++ image_reconstruction/recon_image_naturalImage_VGG19_DGN_GD.py
@@ -1,9 +1,9 @@
-'''Script for Deep Image Reconstruction.
+'''Script for Deep Image Reconstruction for natural images
 Modified from: https://github.com/KamitaniLab/DeepImageReconstruction
 
-This is mainly used for the reconstruction of artificial images.
+This is mainly used for the reconstruction of natural images.
 
-- Reconstruction algorithm: L-BFGS (without deep generator network)
+- Reconstruction algorithm: Deep generator network + Gradient descent
 - DNN: Caffe VGG19
 - Layers: All conv and fc layers
 - Images: ImageNetTest
@@ -21,14 +21,12 @@ import numpy as np
 import PIL.Image
 import scipy.io as sio
 
-from icnn.icnn_lbfgs import reconstruct_image
+from icnn.icnn_dgn_gd import reconstruct_image
 from icnn.utils import clip_extreme_value, estimate_cnn_feat_std, normalise_img
-
 import pandas as pd
 
 from bdpy.distcomp import DistComp
 from bdpy.util import makedir_ifnot
-
 
 # Settings ###################################################################
 
@@ -36,7 +34,9 @@ from bdpy.util import makedir_ifnot
 caffe.set_mode_gpu()
 caffe.set_device(0)
 
-converter_param = '../params/converter_params.csv'
+converter_param = './params/converter_params_1conversion.csv'
+# To use converter for all subject pairs and with varying training samples, uncommented the line below.
+# converter_param = './params/converter_params.csv'
 
 df_param = pd.read_csv(converter_param)
 
@@ -44,10 +44,8 @@ df_param = pd.read_csv(converter_param)
 decoded_features_result_dir = '../feat_results/ncc_predict_feat'
 decoded_features_dir = os.path.join(decoded_features_result_dir, 'decoded_features')
 
-#decode_feature_filename = lambda net, layer, subject, roi, image_label: os.path.join(decoded_features_dir, net, layer, subject, roi, image_label + '.mat')
 def decode_feature_filename(decoded_features_dir, net, layer, conversion, roi, method, num_samples, alpha, image_label):
     return os.path.join(decoded_features_dir, net, layer, conversion, roi, method, str(num_samples), str(alpha), image_label + '.mat')
-
 # Data settings
 results_dir = './recon_img_results'
 network = 'caffe/VGG_ILSVRC_19_layers'
@@ -91,9 +89,19 @@ image_label_list = ['Img0001',
                     'Img0037',
                     'Img0038',
                     'Img0039',
-                    'Img0040']
+                    'Img0040',
+                    'Img0041',
+                    'Img0042',
+                    'Img0043',
+                    'Img0044',
+                    'Img0045',
+                    'Img0046',
+                    'Img0047',
+                    'Img0048',
+                    'Img0049',
+                    'Img0050']
 
-max_iteration = 200
+n_iteration = 200
 
 # Main #######################################################################
 
@@ -112,11 +120,24 @@ net = caffe.Classifier(prototxt_file, model_file, mean=img_mean, channel_swap=ch
 h, w = net.blobs['data'].data.shape[-2:]
 net.blobs['data'].reshape(1, 3, h, w)
 
-# Initial image for the optimization (here we use the mean of ilsvrc_2012_mean.npy as RGB values)
-initial_image = np.zeros((h, w, 3), dtype='float32')
-initial_image[:, :, 0] = img_mean[2].copy()
-initial_image[:, :, 1] = img_mean[1].copy()
-initial_image[:, :, 2] = img_mean[0].copy()
+# Generator network
+model_file = './net/generator_for_inverting_fc7/generator.caffemodel'
+prototxt_file = './net/generator_for_inverting_fc7/generator.prototxt'
+net_gen = caffe.Net(prototxt_file, model_file, caffe.TEST)
+input_layer_gen = 'feat'        # Input layer for generator net
+output_layer_gen = 'generated'  # Output layer for generator net
+
+# Feature size for input layer of the generator net
+feat_size_gen = net_gen.blobs[input_layer_gen].data.shape[1:]
+num_of_unit = net_gen.blobs[input_layer_gen].data[0].size
+
+# Upper bound for input layer of the generator net
+bound_file = './data/act_range/3x/fc7.txt'
+upper_bound = np.loadtxt(bound_file, delimiter=' ', usecols=np.arange(0, num_of_unit), unpack=True)
+upper_bound = upper_bound.reshape(feat_size_gen)
+
+# Initial features for the input layer of the generator (we use a 0 vector as initial features)
+initial_gen_feat = np.zeros_like(net_gen.blobs[input_layer_gen].data[0])
 
 # Feature SD estimated from true CNN features of 10000 images
 feat_std_file = './data/estimated_vgg19_cnn_feat_std.mat'
@@ -137,11 +158,35 @@ opts = {
     # The loss function type: {'l2','l1','inner','gram'}
     'loss_type': 'l2',
 
-    # The maximum number of iterations
-    'maxiter': max_iteration,
+    # The total number of iterations for gradient descend
+    'iter_n': n_iteration,
 
-    # The initial image for the optimization (setting to None will use random noise as initial image)
-    'initial_image': initial_image,
+    # Learning rate
+    'lr_start': 2.,
+    'lr_end': 1e-10,
+
+    # Gradient with momentum
+    'momentum_start': 0.9,
+    'momentum_end': 0.9,
+
+    # Decay for the features of the input layer of the generator after each
+    # iteration
+    'decay_start': 0.01,
+    'decay_end': 0.01,
+
+    # Name of the input layer of the generator (str)
+    'input_layer_gen': input_layer_gen,
+
+    # Name of the output layer of the generator (str)
+    'output_layer_gen': output_layer_gen,
+
+    # Upper and lower boundary for the input layer of the generator
+    'feat_upper_bound': upper_bound,
+    'feat_lower_bound': 0.,
+
+    # The initial features of the input layer of the generator (setting to
+    # None will use random noise as initial features)
+    'initial_gen_feat': initial_gen_feat,
 
     # A python dictionary consists of channels to be selected, arranged in
     # pairs of layer name (key) and channel numbers (value); the channel
@@ -158,16 +203,15 @@ opts = {
     #units for all layers;
     'mask': None,
 
-    # Display the information on the terminal or not
-    'disp': True
+    # Display the information on the terminal for every n iterations
+    'disp_every': 1
 }
 
 # Save the optional parameters
 with open(os.path.join(save_dir_root, 'options.pkl'), 'w') as f:
     pickle.dump(opts, f)
 
-# Reconstrucion --------------------------------------------------------------
-
+    # Reconstrucion --------------------------------------------------------------
 for index, row in df_param.iterrows():
     src = str(row['Source'])
     trg = str(row['Target'])
@@ -192,12 +236,13 @@ for index, row in df_param.iterrows():
         continue
 
     makedir_ifnot('tmp_recon')
-    recon_id = '-'.join(['shape',conversion, roi, method, str(num_samples), str(alpha)])
+    recon_id = '-'.join([conversion, roi, method, str(num_samples), str(alpha)])
     dist = DistComp(lockdir='tmp_recon', comp_id=recon_id)
     if dist.islocked():
         print('%s is already running. Skipped.' % recon_id)
         continue
     dist.lock()
+
 
     for image_label in image_label_list:
 
@@ -241,9 +286,11 @@ for index, row in df_param.iterrows():
 
         # Reconstruction
         snapshots_dir = os.path.join(save_dir, 'snapshots', 'image-%s' % image_label)
-        recon_img, loss_list = reconstruct_image(features, net,
+        norm_and_clip = lambda x: normalise_img(clip_extreme_value(x, pct=4))
+        recon_img, loss_list = reconstruct_image(features, net, net_gen,
                                                  save_intermediate=False,
                                                  save_intermediate_path=snapshots_dir,
+                                                 save_intermediate_postprocess=norm_and_clip,
                                                  **opts)
 
         # Save the results
@@ -258,3 +305,4 @@ for index, row in df_param.iterrows():
         # within [0,255].
         save_name = 'recon_img_normalized' + '-' + image_label + '.jpg'
         PIL.Image.fromarray(normalise_img(clip_extreme_value(recon_img, pct=4))).save(os.path.join(save_dir, save_name))
+    dist.unlock()
